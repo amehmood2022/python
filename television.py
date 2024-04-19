9f2e66e3c24ede2d4b8f477d94eb0c741da7633a
amehmood2022 <amehmood@unomaha.edu>
2024-04-18 21:47:49 -0500

    Second Commit

--- television.py
+++ television.py
@@ -7,6 +7,7 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
+        """Initialize the television with default values"""
         self.__status = False
         self.__muted = False
         self.__volume = self.MIN_VOLUME
@@ -14,9 +15,11 @@ class Television:
         self.__previous_volume = self.MIN_VOLUME  # To store the volume before muting
 
     def power(self):
+        """Toggle the power of the television"""
         self.__status = not self.__status
 
     def mute(self):
+        """Toggle the mute of the television"""
         if self.__status:
             if self.__muted:
                 self.__volume = self.__previous_volume  # Restore previous volume
@@ -26,14 +29,17 @@ class Television:
             self.__muted = not self.__muted
 
     def channel_up(self):
+        """Increase the channel by 1"""
         if self.__status:
             self.__channel = (self.__channel + 1) % (self.MAX_CHANNEL + 1)
 
     def channel_down(self):
+        """Decrease the channel by 1"""
         if self.__status:
             self.__channel = (self.__channel - 1) % (self.MAX_CHANNEL + 1)
 
     def volume_up(self):
+        """Increase the volume by 1"""
         if self.__status:
             if self.__muted:
                 self.mute()  # Unmute before adjusting volume
@@ -41,12 +47,14 @@ class Television:
                 self.__volume += 1
 
     def volume_down(self):
+        """Decrease the volume by 1"""
         if self.__status:
             if self.__muted:
                 self.mute()  # Unmute before adjusting volume
             if self.__volume > self.MIN_VOLUME:
                 self.__volume -= 1
-
+#
     def __str__(self):
+        """Print the television object"""
         power_status = "True" if self.__status else "False"
         return f"Power = {power_status}, Channel = {self.__channel}, Volume = {self.__volume}"
